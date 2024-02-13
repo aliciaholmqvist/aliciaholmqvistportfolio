67df6e28e5a8091bef662fa079ca7cbba5cd3d4a
FilipTakvam <filip.takvam@hotmail.com>
2024-02-13 22:12:36 +0100

    &apos;

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -20,7 +20,7 @@ export default function About() {
             <p><span>Hello!</span><br />
               I am Alicia, a final year masters student in Interaction Design, Chalmers. I conducted my bachelors in Industrial Design Engineering at Chalmers, which share the user-centered aspect of Interaction design, which I love. As of now I focus my expertise more on digital products and user experience. Combinding design thinking and creativity with the structure of engineering, gives me comfort in that I can produce qualitative designs that I am proud of. In the end, my passion is about creating holistic solutions for end users, whether it be a digital product, a physical product or both!
               Take a look at my portfolio to find some of the projects from my bachelors and masters education!
-              Don't hesitate to contact me!</p>
+              Don&apos;t hesitate to contact me!</p>
             <div className={styles.cheesy}>
               <h2 className={styles.title}>Designer<br></br>by heart,</h2>
               <h2 className={styles.title}>Engineer<br></br>by practice.</h2>
